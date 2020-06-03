8ea700b6fc7873ac8d039139b2b37404795c238c
AmyTroutman <amyrtroutman@gmail.com>
2020-06-02 20:09:23 -0500

    Generated all components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,16 @@ import { DateTimeComponent } from './date-time/date-time.component';
 import { MessagePreviewComponent } from './message-preview/message-preview.component';
 import { TaskItemComponent } from './task-item/task-item.component';
 import { TopNavComponent } from './top-nav/top-nav.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { CommentsComponent } from './comments/comments.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { OrdersComponent } from './orders/orders.component';
+import { TicketsComponent } from './tickets/tickets.component';
+import { AreaChartComponent } from './area-chart/area-chart.component';
+import { DonutChartComponent } from './donut-chart/donut-chart.component';
+import { TasksPanelComponent } from './tasks-panel/tasks-panel.component';
+import { TransactionsPanelComponent } from './transactions-panel/transactions-panel.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +23,17 @@ import { TopNavComponent } from './top-nav/top-nav.component';
     DateTimeComponent,
     MessagePreviewComponent,
     TaskItemComponent,
-    TopNavComponent
+    TopNavComponent,
+    SideNavComponent,
+    CommentsComponent,
+    TasksComponent,
+    OrdersComponent,
+    TicketsComponent,
+    AreaChartComponent,
+    DonutChartComponent,
+    TasksPanelComponent,
+    TransactionsPanelComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule
